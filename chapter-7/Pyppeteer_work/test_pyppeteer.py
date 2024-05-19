429f6f421ba91499f655a5e077a4476fdc1e4341
Sprite <solospecter666@qq.com>
2024-05-19 10:10:46 +0800

    commit

--- chapter-7/Pyppeteer_work/test_pyppeteer.py
+++ chapter-7/Pyppeteer_work/test_pyppeteer.py
@@ -3,4 +3,4 @@ import pyppeteer.chromium_downloader
 print(pyppeteer.chromium_downloader.chromiumExecutable.get("win64"))
 print(pyppeteer.chromium_downloader.downloadURLs.get("win64"))
 
-#无法解决连接https://storage.googleapis.com下载文件的问题
+#无法解决连接https://storage.googleapis.com/ 下载文件的问题
